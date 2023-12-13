7c07b3b0414153d283e60e0927aaff4be4f69278
rasmit <rasmit2022@navgurukul.org>
2023-12-13 15:32:28 +0530

    Adding Changes

--- services/buses.services.js
+++ services/buses.services.js
@@ -5,10 +5,40 @@ const CheckValidation = function (busData) {
         const statusData = {
             status: false,
             statusCode: 404,
-            data: {message:"please enter all details"}
+            data: { message: "please enter all details" }
         }
         return statusData
     }
+
+    if (isNaN(busData.total_seats) || busData.total_seats<=0) {
+        const statusData = {
+            status: false,
+            statusCode: 404,
+            data: { message: "Total seats must be a positive number." }
+        };
+        return statusData;
+    }
+
+    const validDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    for (let i = 0; i < validDays.length; i++) {
+        if (busData.days === (validDays[i])) {
+            const statusData = {
+                status: false,
+                statusCode: 404,
+                data: { message: "Invalid days. Please provide valid days." }
+            };
+            return statusData;
+        }
+    }
+    if (!Array.isArray(busData.days) || busData.days.length === 0) {
+        const statusData = {
+            status: false,
+            statusCode: 404,
+            data: { message: "Invalid days. Please provide an array of valid days." }
+        };
+        return statusData;
+    }
+
     return busModels.insertBusDetails(busData)
 }
 
