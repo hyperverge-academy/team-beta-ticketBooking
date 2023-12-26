de79b42ae2850f876684fe68bdfc3170f970c0a7
SAYALI <sayalighadigaonkar22@navgurukul.org>
2023-12-26 12:13:58 +0530

    update user bookings

---
+++ constants/resp.constants.js
@@ -0,0 +1,8 @@
+const respConst = {
+    internalServerError : {
+        'Message':"Internal Server Error. we are looking into this server"},        
+        'Status':"Sattus False",
+        'StatusCode':500
+}
+
+module.exports = { respConst };
--- models/users.model.js
+++ models/users.model.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require("mongodb");
+const dbConst = require('../constants/db.constants')
+const respConst = require('../constants/resp.constants')
 const uri = "mongodb://localhost:27017";
 
 const saveBooking = async (bookingData) => {
@@ -21,20 +23,25 @@ const saveBooking = async (bookingData) => {
 }
 
 const getAllBookings = async (userId) => {
-    const client = new MongoClient(uri);
+    const client = new MongoClient(dbConst.uri);
     try {
-        const database = client.db("busBookings");
-        const bookingsCollection = database.collection("bookings");
+        const database = client.db(dbConst.dbName);
+        const bookingsCollection = database.collection(dbConst.busCollection);
         const query = {userId:userId}
 
         const allBookings = await bookingsCollection.find(query);
         if ((await bookingsCollection.countDocuments(query))===0){
+          return { message: 'No bookings found for the specified user.' };
         }
         const bookingsArray = []
         for await(const doc of allBookings){
           bookingsArray.push(doc)        
         }
         return bookingsArray;        
+    }
+    catch (error) {
+        console.error('Error retrieving bookings:', error);
+        return respConst.internalServerError
     } 
     finally {
         await client.close();
