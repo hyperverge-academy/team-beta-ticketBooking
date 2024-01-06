c87f2ba7bf06ae608987f8129cb1f582514fde4f
SAYALI <sayalighadigaonkar22@navgurukul.org>
2024-01-06 15:05:54 +0530

    changes

--- constants/db.constants.js
+++ constants/db.constants.js
@@ -1,9 +1,7 @@
-
 const dbConst = {
     dbName:"busBookings",
     bookingCollection:"bookings",
     userCollection : "users",
     uri:"mongodb://localhost:27017",
 }
-
 module.exports = dbConst;
